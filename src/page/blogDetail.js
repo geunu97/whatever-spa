6b81be55fc502268fa94c4d6d7d57f4bd5258cfc
geunu <olo90632951@gmail.com>
2024-02-02 10:55:00 +0900

    feat: 라우팅 기능 추가

---
+++ src/page/blogDetail.js
@@ -0,0 +1,3 @@
+export const BlogDetail = () => {
+  return `<div>상세페이지입니다.</div>`;
+};
---
+++ src/page/home.js
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return `
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#blog">블로그</a></li>
+        <li><a href="#blog/detail">게시글 상세보기</a></li>        
+      </ul>
+    </nav>
+  `
+};
