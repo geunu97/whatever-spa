fc263e4b6b7aa58003ac972197c4c65ee48a3c31
geunu <olo90632951@gmail.com>
2024-02-04 12:35:12 +0900

    style: header 메뉴 활성화 시 색상 추가

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,28 @@
+import { hashLocation } from '../utils/hashLocation.js';
+
 export const Header = () => {
+  console.log(hashLocation.path());
+
+  const routes = [
+    { path: '#', text: '홈' },
+    { path: '#blog', text: '블로그' },    
+  ];
+
+  const getActiveClass = (route) => {
+    const mainPath = hashLocation.path().split('/')[0];
+    return route.path === `#${mainPath}` ? 'active' : '';
+  }
+
   return `    
     <div class="header">
-      <img src="/assets/img/mainLogo.png" alt="Main Logo">
+      <a href="#"><img src="/assets/img/mainLogo.png" alt="Main Logo"></a>      
       <nav>
-        <ul>          
-          <li><a href="#blog">블로그</a></li>     
+        <ul>
+          ${routes.map(route => (
+            `
+              <li><a href=${route.path} class=${getActiveClass(route)}>${route.text}</a></li>                             
+            `          
+          )).join('')}          
         </ul>      
       </nav>         
     </div>
