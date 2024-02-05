ea55f1942428659abb8b3751481488bdf6ac5797
geunu <olo90632951@gmail.com>
2024-02-05 12:15:47 +0900

    fix: refresh 함수 파라미터 수정

--- src/utils/render.js
+++ src/utils/render.js
@@ -39,7 +39,7 @@ export const render = {
   refresh: (HTMLString, selector) => {
     const parser = new DOMParser();
     const doc = parser.parseFromString(HTMLString, 'text/html');
-    const content = doc.querySelector('.blog').innerHTML;
+    const content = doc.querySelector(selector).innerHTML;
 
     const $target = document.querySelector(selector)    
     $target.innerHTML = content;
